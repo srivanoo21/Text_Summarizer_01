cb44dcc2bb8e52a92bcf99366e27b0d72b0154ef
Anoop Srivastava <srivanoo21@gmail.com>
2023-06-04 22:34:56 +0530

    requirements and installation done

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "testSummarizer"
+project_name = "textSummarizer"
 
 
 # Below list of files need to be created
